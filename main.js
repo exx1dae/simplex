31eb426c46267b83c9d6029edfb795008ef77c71
exxidae <sarkis200349@gmail.com>
2024-02-29 00:29:37 +0300

    final commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const matrix2 = [
+const matrix1 = [
     [   'F',   2, 1, 0, 0, 0, 0, 0, 'max' ],
     [ 'базис', 1,  2, 3, 4, 5, 6, 'b', 'q' ],
     [    3,   -4, -6, 1, 0, 0, 0, -20 ],
@@ -8,7 +8,7 @@ const matrix2 = [
     [   'd',   0,  0, 0, 0, 0, 0,  0 ]
 ]
 
-const matrix1 = [
+const matrix2 = [
     [   'F',   2, 1, 0, 0, 0, 0, 0, 'min' ],
     [ 'базис', 1,  2, 3, 4, 5, 6, 'b', 'q' ],
     [    3,   -4, -6, 1, 0, 0, 0, -20 ],
@@ -28,55 +28,84 @@ const matrix3 = [
 ]
 
 const matrix4 = [
-    [   'F',   2,  3, 0, 0, 0,  0, 'max' ],
+    [   'F',   1,  3, 0, 0, 0,  0, 'max' ],
     [ 'базис', 1,  2, 3, 4, 5, 'b', 'q' ],
     [    3,    1,  2, 1, 0, 0,  8 ],
-    [    4,    1,  1, 0, 1, 0,  6 ],
+    [    4,    3,  1, 0, 1, 0,  6 ],
     [    5,   -2, -3, 0, 0, 1, -3 ],
-    [   'd',   0,  0, 0, 0, 0,  0 ]
-]
-
-const matrix6 = [
-    [   'F',   2,  4, 0, 0, 0,  0, 'min' ],
-    [ 'базис', 1,  2, 3, 4, 5, 'b', 'q' ],
-    [    3,    1,  2, 1, 0, 0,  8 ],
-    [    4,    1,  3, 0, 1, 0,  6 ],
-    [    5,   -1, -2, 0, 0, 1, -3 ],
     [   'd',   0,  0, 0, 0, 0,  0 ]
 ]
 
 const matrix5 = [
-    [   'F',   2,  1, 0, 0, 0,  0, 'max' ],
+    [   'F',   4,  3, 0, 0, 0,  0, 'min' ],
     [ 'базис', 1,  2, 3, 4, 5, 'b', 'q' ],
-    [    3,    -1,  -2, 1, 0, 0,  8 ],
-    [    4,    -1,  -3, 0, 1, 0,  -6 ],
+    [    3,    1,  4, 1, 0, 0,  8 ],
+    [    4,    3,  1, 0, 1, 0,  6 ],
+    [    5,   -4, -3, 0, 0, 1, -3 ],
+    [   'd',   0,  0, 0, 0, 0,  0 ]
+]
+
+const matrix6 = [
+    [   'F',   3,  3, 0, 0, 0,  0, 'max' ],
+    [ 'базис', 1,  2, 3, 4, 5, 'b', 'q' ],
+    [    3,    -1,  -2, 1, 0, 0,  -10 ],
+    [    4,    -2,  -1, 0, 1, 0,  -6 ],
     [    5,   -2, -3, 0, 0, 1, -3 ],
     [   'd',   0,  0, 0, 0, 0,  0 ]
 ]
 
-const matrix = [
-    [   'F',   2,  1, 0, 0, 0,  0, 'min' ],
+const matrix7 = [
+    [   'F',   2,  6, 0, 0, 0,  0, 'min' ],
     [ 'базис', 1,  2, 3, 4, 5, 'b', 'q' ],
-    [    3,    1,  2, 1, 0, 0,  4 ],
-    [    4,    2,  1, 0, 1, 0,  6 ],
-    [    5,   -1, -2, 0, 0, 1, -8 ],
+    [    3,    1,  2, 1, 0, 0,  8 ],
+    [    4,    1,  3, 0, 1, 0,  6 ],
+    [    5,   -1, -1, 0, 0, 1, -10 ],
     [   'd',   0,  0, 0, 0, 0,  0 ]
 ]
 
+const matrixes = [
+    matrix1,
+    matrix2,
+    matrix3,
+    matrix4,
+    matrix5,
+    matrix6,
+    matrix7
+]
 
-const INDEX_COL_B = matrix[1].findIndex((value) => value === 'b');
-const INDEX_COL_Q = matrix[1].findIndex((value) => value === 'q');
-const INDEX_COL_EXTREMUM = matrix[0].findIndex((value) => value === 'max' || value === "min" );
-const EXTREMUM = matrix[0][INDEX_COL_EXTREMUM];
+let INDEX_COL_B;
+let INDEX_COL_Q;
+let INDEX_COL_EXTREMUM;
+let EXTREMUM;
 
-simplex(matrix);
+
+for (let matrix of matrixes) {
+    INDEX_COL_B = matrix[1].findIndex((value) => value === 'b');
+    INDEX_COL_Q = matrix[1].findIndex((value) => value === 'q');
+    INDEX_COL_EXTREMUM = matrix[0].findIndex((value) => value === 'max' || value === "min" );
+    EXTREMUM = matrix[0][INDEX_COL_EXTREMUM];
+
+    
+    simplex(matrix);
+    console.log('------------------------');
+}
+
+function getColumn(matrix, columnIndex) {
+    let column = [];
+
+    for (let i = 2; i < matrix.length; i++) {
+        column.push(matrix[i][columnIndex]);
+    }
+
+    return column;
+}
 
 function hasBNegative(matrix) {
-    for (let row = 2; row < matrix.length - 1; row++) {
-        if (matrix[row][INDEX_COL_B] < 0) {
-            console.log("В столбце b присутствуют отрицательные значения.")
-            return true;
-        }
+    let bColumn = getColumn(matrix, INDEX_COL_B);
+
+    if (bColumn.find((element) => element < 0)) {
+        console.log("В столбце B присутствует отрицательное число!");
+        return true;
     }
     return false;
 }
@@ -98,22 +127,33 @@ function getIndexesOfMax(matrix) {
 
     max = 0;
 
+
+
+    if (matrix[rowMaxIndex].slice(1, -1).every((element) => element >= 0)) {
+        return false;
+    }
+
     for (let i = 1; i < matrix[rowMaxIndex].length - 1; i++) {
         current = matrix[rowMaxIndex][i];
 
         if (current < 0 && Math.abs(current) > max) {
-            max = current;
+            max = Math.abs(current);
             colMaxIndex = i;
         }
+        
     }
 
     return [ rowMaxIndex, colMaxIndex ];
 }
 
 function swapBazis(matrix, indexes) {
+    if (indexes === false) {
+        return false;
+    }
     let [row, col] = indexes;
 
     matrix[row][0] = matrix[1][col];
+    return true;
 }
 
 function newMatrix(matrix, indexes) {
@@ -195,7 +235,6 @@ function calculateQ(matrix) {
     }
 
     if (array.every((element) => element === Infinity)) {
-        console.log(array);
         return false;
     }
     return true;
@@ -246,10 +285,18 @@ function calculateFunc(matrix) {
 
 function simplex(matrix) {
     let isInfinitySolve = false;
+    let hasSolution = true;
 
-    if (hasBNegative(matrix)) {
-        swapBazis(matrix, getIndexesOfMax(matrix));
-        newMatrix(matrix, getIndexesOfMax(matrix));
+    while (hasBNegative(matrix)) {
+        let indexes = getIndexesOfMax(matrix);
+
+        if (indexes === false) {
+            hasSolution = false;
+            break;
+        }
+
+        swapBazis(matrix, indexes) 
+        newMatrix(matrix, indexes);
     }
 
     solveD(matrix);
@@ -263,7 +310,7 @@ function simplex(matrix) {
         }
     } 
     else {
-        while (checkOptimality(matrix)) {
+        while (checkOptimality(matrix) && hasSolution) {
             iteration(matrix);
         }
     }
@@ -272,7 +319,11 @@ function simplex(matrix) {
 
     if (isInfinitySolve) {
         console.log("Функция не ограничена");
-    } else {
+    } 
+    else if (!hasSolution) {
+        console.log('Нет решений')
+    }
+    else {
         calculateFunc(matrix);
     }
 }
